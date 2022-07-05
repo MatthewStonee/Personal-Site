d286a79817e3ad51bbcec222658219f17490b34e
Matthew <mattstone01s@gmail.com>
2022-07-04 22:06:00 -0400

    center homepage content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,54 @@
 <template>
   <div class="header">
     <div class="intro">
-      <h1>Hi 👋 I’m Matthew Stone!</h1>
-      <h2>I’m a software engineer at Bolt and graduate of the University of Central Florida</h2>
-    </div>
-
-
-    <div class="container-1">
-      <div class="Exp">
-        <p>🎓  B.S. Computer Science @ University of Central Florida</p>
-        <p class="p2">📍 Grew up in Central Florida</p>
-        <p>💪 Love to workout, game, and trade stocks</p>
-        <p class="p2">⌨️ There are some simple web apps on this website that I built while learning different aspects of web development!</p>
+      <div>
+        <h1>Hi 👋 I’m Matthew Stone!</h1>
+        <h2>I’m a software engineer at Bolt and graduate of the University of Central Florida</h2>
       </div>
 
-      <div class="image">
-        <v-sheet
-            color="grey lighten-1"
-            rounded
-            elevation="8"
-            max-width="325"
-        >
-          <v-img :src="images.profile"></v-img>
-        </v-sheet>
+
+      <div class="container-1">
+        <div class="Exp">
+          <p>🎓  B.S. Computer Science @ University of Central Florida</p>
+          <p class="p2">📍 Grew up in Central Florida</p>
+          <p>💪 Love to workout, game, and trade stocks</p>
+          <p class="p2">⌨️ There are some simple web apps on this website that I built while learning different aspects of web development!</p>
+        </div>
+
+        <div class="image">
+          <v-sheet
+              color="grey lighten-1"
+              rounded
+              elevation="8"
+              max-width="325"
+          >
+            <v-img :src="images.profile"></v-img>
+          </v-sheet>
+        </div>
       </div>
+
     </div>
-
-
-
-
   </div>
 
 </template>
 
 <style scoped>
 
-.header {
-  display: block;
+.intro {
   width: 100%;
-  margin: auto;
   padding: 45px;
   max-width: 815px;
   text-align: justify;
   font-family: Prompt, monospace;
 }
 
-.intro {
-  margin: auto;
+.header {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 100vh;
 }
 
 h1 {
